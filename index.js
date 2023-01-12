9b7ae4d62aaa73daf0313843c5b3ab6b6d21ddf3
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-12 20:25:55 -0300

    configure initial sequelize

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
-
+require('dotenv').config();
 const app = express()
 
-const port = proce.env.PORT || 8080
+const port = process.env.PORT || 8080
 
 app.listen(port, () => {
     console.log(`Server runing on port ${port}`)
