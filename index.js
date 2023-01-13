666f7c9bbe49d3ac7b50b482eecf4ff27eb9ea94
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-13 10:48:17 -0300

    install nodemon

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express()
 const setupRoutes = require('./src/main/routers')
 const port = process.env.PORT || 8080
 
-setupRoutes(app)
+app.use(express.json())
 app.listen(port, () => {
     console.log(`Server runing on port ${port}`)
 })
+setupRoutes(app)
---
+++ src/main/middlewares/validation.js
@@ -0,0 +1,5 @@
+
+module.exports = function validation(schema, req, res, next) {
+
+    
+}
