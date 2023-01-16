4d4b9a4617696c320321e48b45db62646244a253
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-16 10:52:41 -0300

    verify fields

--- src/domain/useCases/auth-user-useCase.js
+++ src/domain/useCases/auth-user-useCase.js
@@ -1,10 +1,12 @@
 const { updateUser, findByEmail } = require('../../infra/repositorys/users-repository')
 const { compare } = require('../../utils/helpers/encrypter')
-const { MissingParamError, NotFoundError, Unauthorized,  } = require('../../utils/helpers/errors')
+const { MissingParamError, NotFoundError, Unauthorized, } = require('../../utils/helpers/errors')
 module.exports = authUserUseCase = async (email, password) => {
+    if (!email) throw new MissingParamError('email')
+    if (!password) throw new MissingParamError('password')
     const user = await findByEmail(email)
     if (!user) throw new NotFoundError('usuario')
     const comparePassword = await compare(password, user.password)
-    if(!comparePassword) throw new Unauthorized()
+    if (!comparePassword) throw new Unauthorized()
     return user
 }
