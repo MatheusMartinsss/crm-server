27167a21325d7ef429ab79ce9a6b7a9fd5b4a898
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-18 16:05:03 -0300

    remove unecessary import from list clientes

--- src/domain/useCases/list-clientes-useCase.js
+++ src/domain/useCases/list-clientes-useCase.js
@@ -1,4 +1,4 @@
-const { findAll, listByVendedorId } = require('../../infra/repositorys/clientes-repository');
+const { findAll} = require('../../infra/repositorys/clientes-repository');
 const { NotFoundError } = require('../../utils/helpers/errors');
 module.exports = listClientesUseCase = async (request) => {
     const { user, query } = request
