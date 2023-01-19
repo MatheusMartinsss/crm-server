328204b171675f2dfe1d4321ff1740924def06a9
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-19 17:16:18 -0300

    add cliente and vendedor data to findUserById method

--- src/infra/repositorys/negociacao-repository.js
+++ src/infra/repositorys/negociacao-repository.js
@@ -1,4 +1,4 @@
-const { Negociacoes } = require('../../models/')
+const { Negociacoes, Clientes, User } = require('../../models/')
 const { NotFoundError } = require('../../utils/helpers/errors')
 const QuerySequelize = require('../helpers/query-builder')
 const queryBuilder = new QuerySequelize()
@@ -25,11 +25,13 @@ const findAll = async (user, query) => {
     return Negociacoess
 }
 const findById = async (id) => {
-    const _Negociacoes = await Negociacoes.findOne({
-        where: {
-            id
-        }
-    })
+    queryBuilder
+        .setWhere({ id: id })
+        .setIncludes([{ model: Clientes, as: 'Cliente' }, { model: User, as: 'Vendedor' }])
+        .setIncludesAttributesExclude(Clientes, ['vendedor_id'])
+        .setIncludesAttributesExclude(User, ['password', 'token', 'role'])
+
+    const _Negociacoes = await Negociacoes.findOne(queryBuilder.getQuery())
     return _Negociacoes
 }
 
