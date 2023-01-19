c0dc54c0e4d1911bcd45c53330c3c0709b1663cf
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-19 16:54:16 -0300

    add validation to cliente id

--- src/domain/useCases/negociacoes/create-negociacao-useCase.js
+++ src/domain/useCases/negociacoes/create-negociacao-useCase.js
@@ -1,8 +1,12 @@
 const { create } = require('../../../infra/repositorys/negociacao-repository')
-const {  MissingParamError } = require('../../../utils/helpers/errors')
+const findClienteUseCase = require('../clientes/find-cliente-useCase')
+const { MissingParamError } = require('../../../utils/helpers/errors')
 module.exports = createNegociacaoUseCase = async (user, body) => {
     const requiredFields = ['name', 'description']
     body.vendedor_id = user.id
+    if (body.cliente_id) {
+        await findClienteUseCase(user, body.cliente_id)
+    }
     for (const field in requiredFields) {
         if (!body[requiredFields[field]]) {
             throw new MissingParamError(requiredFields[field])
