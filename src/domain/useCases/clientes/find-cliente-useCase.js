93043b21651fb909a9773cd71a593e80ace05705
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-19 16:54:27 -0300

    fix find cliente useCase

--- src/domain/useCases/clientes/find-cliente-useCase.js
+++ src/domain/useCases/clientes/find-cliente-useCase.js
@@ -1,17 +1,18 @@
 const { findById } = require('../../../infra/repositorys/clientes-repository')
 const { MissingParamError, NotFoundError, Unauthorized } = require('../../../utils/helpers/errors')
 module.exports = findUserUseCase = async (user, id) => {
-    
+
     if (!id)
         throw new MissingParamError('id')
 
     const cliente = await findById(id)
 
+    if (!cliente)
+        throw new NotFoundError('Cliente')
+
     if (user.role != 'admin' && cliente.vendedor_id != user.id)
         throw new Unauthorized()
 
-    if (!cliente)
-        throw new NotFoundError('Cliente não encontrado!')
 
     return cliente
 }
