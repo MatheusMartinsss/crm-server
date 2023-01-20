27c9708fa5ff0a5edd592f6e7654ac75e45c7d3c
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-20 11:34:53 -0300

    fix verify group already exists

--- src/domain/useCases/groups/create-group-useCase.js
+++ src/domain/useCases/groups/create-group-useCase.js
@@ -1,17 +1,16 @@
-const { create } = require('../../../infra/repositorys/groups-repository')
+const { create, findByName } = require('../../../infra/repositorys/groups-repository')
 const { MissingParamError, FieldAlreadyInUse } = require('../../../utils/helpers/errors')
-const findGroupByNameUseCase = require('./find-group-by-name-useCase')
 module.exports = createGroupUseCase = async (body) => {
 
     const requiredFields = ['name', 'description']
-    
+
     for (const field in requiredFields) {
         if (!body[requiredFields[field]]) {
             throw new MissingParamError(requiredFields[field])
         }
     }
  
-    const verifyNameAlreadyExists = await findGroupByNameUseCase(name)
+    const verifyNameAlreadyExists = await findByName(body.name)
  
     if (verifyNameAlreadyExists)
         throw new FieldAlreadyInUse('name', body.name)
