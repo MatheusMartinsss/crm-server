17e9244d30c1a2ba91a6551cceca2f13ca733118
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-20 16:17:53 -0300

    add group id to update negociacao

--- src/domain/useCases/negociacoes/update-negociacao-useCase.js
+++ src/domain/useCases/negociacoes/update-negociacao-useCase.js
@@ -1,6 +1,7 @@
 const findNegociacaoUseCase = require('./find-negociacao-useCase')
 const findClienteUseCase = require('../clientes/find-cliente-useCase')
 const findUserUseCase = require('../user/find-user-useCase')
+const findGroupByIdUseCase = require('../groups/find-group-by-id-useCase')
 const { updateNegociacao } = require('../../../infra/repositorys/negociacao-repository')
 const { MissingParamError, Unauthorized } = require('../../../utils/helpers/errors')
 module.exports = updateNegociacaoUseCase = async (user, id, body) => {
@@ -10,16 +11,20 @@ module.exports = updateNegociacaoUseCase = async (user, id, body) => {
 
     await findNegociacaoUseCase(user, id)
 
-    if (body.cliente_id) {
+    if (body?.cliente_id) {
         await findClienteUseCase(user, body.cliente_id)
     }
-
-    if (body.vendedor_id && user.role != 'admin')
+    if (body?.vendedor_id && user.role != 'admin')
         throw new Unauthorized()
 
-    if (body.vendedor_id) {
+    if (body?.vendedor_id) {
         await findUserUseCase(body.vendedor_id)
     }
+    if (body?.group_id) {
+        await findGroupByIdUseCase(body.group_id)
+    }
+
     const negociacao = await updateNegociacao(id, body)
+
     return negociacao
 }
