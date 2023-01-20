1e21dd93d04761a5ea08331e59c4e0f3f78ac11c
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-20 08:06:56 -0300

    fix query negociacao repository

--- src/infra/repositorys/negociacao-repository.js
+++ src/infra/repositorys/negociacao-repository.js
@@ -1,7 +1,7 @@
 const { Negociacoes, Clientes, User } = require('../../models/')
 const { NotFoundError } = require('../../utils/helpers/errors')
 const QuerySequelize = require('../helpers/query-builder')
-const queryBuilder = new QuerySequelize()
+
 const create = async (data) => {
     const _Negociacoes = await Negociacoes.create(data)
     return _Negociacoes
@@ -15,16 +15,21 @@ const findByEmail = async (email) => {
     return _Negociacoes
 }
 const findAll = async (user, query) => {
+    const queryBuilder = new QuerySequelize()
     if (user.role != 'admin') {
         queryBuilder.setWhere({ vendedor_id: user.id })
     }
     if (query.cliente_id) {
         queryBuilder.setWhere({ cliente_id: query.cliente_id })
     }
+    if (query.vendedor_id) {
+        queryBuilder.setWhere({ vendedor_id: user.id })
+    }
     const Negociacoess = await Negociacoes.findAll(queryBuilder.getQuery())
     return Negociacoess
 }
 const findById = async (id) => {
+    const queryBuilder = new QuerySequelize()
     queryBuilder
         .setWhere({ id: id })
         .setIncludes([{ model: Clientes, as: 'Cliente' }, { model: User, as: 'Vendedor' }])
