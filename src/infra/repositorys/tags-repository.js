6be7d54fb6c9097f1356df9a9572c4ce4be91abf
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-21 12:31:01 -0300

    create tags repository base

---
+++ src/infra/repositorys/tags-repository.js
@@ -0,0 +1,31 @@
+const { Tags } = require('../../models')
+const QuerySequelize = require('../helpers/query-builder')
+
+const create = async (body) => {
+    const tag = await Tags.create({ ...body })
+    return tag;
+}
+
+const findByName = async (name) => {
+    const tag = await Tags.findOne({
+        where: {
+            name
+        }
+    })
+    return tag
+}
+
+const findTagById = async (id) => {
+    const tag = await Tags.findOne({
+        where: {
+            id
+        }
+    })
+    return tag
+}
+
+module.exports = {
+    create,
+    findByName,
+    findTagById
+}
