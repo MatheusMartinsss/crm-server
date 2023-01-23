8d442deb4ce1c8b973f57e8a7d5a2d7fd40a8a6f
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-23 09:57:28 -0300

    create join table between tags and negociacoes

---
+++ src/migrations/20230123121516-negociacoes_tags.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('negociacoes_tags', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      negociacao_id: {
+        type: Sequelize.UUID,
+        reference: {
+          model: 'negociacoes', key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      tag_id: {
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'tags', key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+
+    await queryInterface.dropTable('negociacoes_tags')
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
