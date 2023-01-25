2cf6e03f7a1227859bdaf3f2e36008ea350f3a73
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-25 16:46:07 -0300

    add cors

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 require('dotenv').config();
 require('./src/models/index')
 const app = express()
+const cors = require('cors')
 const setupRoutes = require('./src/main/routers')
 const port = process.env.PORT || 8080
 
+app.use(cors())
 app.use(express.json())
 app.listen(port, () => {
     console.log(`Server runing on port ${port}`)
