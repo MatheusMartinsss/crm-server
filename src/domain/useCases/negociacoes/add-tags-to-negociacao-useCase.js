26d7baabed91427c65e169c36e76e6e2c58569ef
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-02-06 09:35:01 -0300

    fix add negociacao use case

--- src/domain/useCases/negociacoes/add-tags-to-negociacao-useCase.js
+++ src/domain/useCases/negociacoes/add-tags-to-negociacao-useCase.js
@@ -2,7 +2,6 @@ const { addTag } = require('../../../infra/repositorys/negociacao-repository')
 const { MissingParamError } = require('../../../utils/helpers/errors')
 const findNegociacaoUseCase = require('./find-negociacao-useCase')
 module.exports = negociacaoAddTagUseCase = async (user, id, body) => {
-
     if (!id)
         throw MissingParamError('id')
 
@@ -11,7 +10,7 @@ module.exports = negociacaoAddTagUseCase = async (user, id, body) => {
 
     const negociacao = await findNegociacaoUseCase(user, id)
 
-    const _negociacao = await addTag(negociacao, body.tags)
+    const _negociacao = await addTag(negociacao, body)
     
     return _negociacao
 }
