619a7ebdbebd126473de2cd4d471ab79b09e0a0a
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-02-27 00:07:28 -0300

    configure production

--- src/main/config/database.js
+++ src/main/config/database.js
@@ -19,11 +19,11 @@ module.exports = {
     logging: true
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.DATABASE_URL,
+    host: process.env.PGHOST,
+    port: '5432',
+    dialect: 'postgres'
   }
 };
